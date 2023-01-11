d92c4ba081fec9105cc00a38df4960474b590651
crisrm128 <96492976+crisrm128@users.noreply.github.com>
2023-01-11 16:04:32 +0100

    Delete ws_helpbot directory

--- ws_helpbot/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cristina/ws_helpbot/src
-# Build directory: /home/cristina/ws_helpbot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("helpbot")
-subdirs("obj_recognition")
--- ws_helpbot/build/obj_recognition/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cristina/ws_helpbot/src/obj_recognition
-# Build directory: /home/cristina/ws_helpbot/build/obj_recognition
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
